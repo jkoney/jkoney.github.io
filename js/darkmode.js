59155a8c1c19801fb3aa5200bc684b64c580a525
Kone Joel <joel.kone@outlook.fr>
2024-03-23 10:07:53 +0000

    chzngement logo

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,40 +1,11 @@
+// Sélectionnez le bouton ou l'élément qui déclenchera le changement de mode
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+// Fonction pour basculer entre le mode sombre et le mode clair
 function toggleDarkMode() {
-    const body = document.body;
-    const navbar = document.querySelector('.navbar');
-    const navLinks = document.querySelectorAll('.nav-link');
-    const customLinks = document.querySelectorAll('.custom-link');
-    const customUnderlines = document.querySelectorAll('.custom-underline');
-
-    // Ajouter ou retirer la classe "dark-mode" du corps
-    body.classList.toggle('dark-mode');
-
-    // Si le corps a la classe "dark-mode", appliquer les styles sombres
-    if (body.classList.contains('dark-mode')) {
-        navbar.classList.add('dark-mode');
-        navLinks.forEach(link => {
-            link.classList.add('dark-mode');
-        });
-        customLinks.forEach(link => {
-            link.classList.add('dark-mode');
-        });
-        customUnderlines.forEach(underline => {
-            underline.classList.add('dark-mode');
-        });
-    } else {
-        // Sinon, rétablir les styles par défaut
-        navbar.classList.remove('dark-mode');
-        navLinks.forEach(link => {
-            link.classList.remove('dark-mode');
-        });
-        customLinks.forEach(link => {
-            link.classList.remove('dark-mode');
-        });
-        customUnderlines.forEach(underline => {
-            underline.classList.remove('dark-mode');
-        });
-    }
+    document.body.classList.toggle('dark-mode');
+    document.querySelector("h2").style="white"
 }
 
-// Ajouter un gestionnaire d'événements pour le bouton de basculement de mode sombre
-const darkModeButton = document.querySelector('.dark-mode-toggle');
-darkModeButton.addEventListener('click', toggleDarkMode);
+// Écoutez l'événement de clic sur le bouton et appelez la fonction toggleDarkMode()
+darkModeToggle.addEventListener('click', toggleDarkMode);
